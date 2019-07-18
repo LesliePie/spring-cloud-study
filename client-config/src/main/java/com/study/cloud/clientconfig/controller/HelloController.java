6223b50d9f00b6d58e60d794d46576df56dbce1c
leslie <15198264880@163.com>
2019-07-18 16:52:00 +0800

    '客户端配置'

---
+++ client-config/src/main/java/com/study/cloud/clientconfig/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.study.cloud.clientconfig.controller;
+
+/**
+ * @program: springCloud
+ * @description:
+ * @author: Leslie
+ * @create: 2019-07-18 16:44
+ **/
+public class HelloController {
+}
