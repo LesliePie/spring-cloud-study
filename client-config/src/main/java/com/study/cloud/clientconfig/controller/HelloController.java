5c81ae8686a094f1b73026147afd792b18e1cacc
leslie <15198264880@163.com>
2019-07-18 17:08:59 +0800

    '修改配置文件'

--- client-config/src/main/java/com/study/cloud/clientconfig/controller/HelloController.java
+++ client-config/src/main/java/com/study/cloud/clientconfig/controller/HelloController.java
@@ -1,10 +1,22 @@
 package com.study.cloud.clientconfig.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @program: springCloud
  * @description:
  * @author: Leslie
  * @create: 2019-07-18 16:44
  **/
+@RestController
 public class HelloController {
+    @Value("${test.hello}")
+    private String hello;
+
+    @GetMapping(value = "/hello")
+    public String from(){
+        return this.hello;
+    }
 }
